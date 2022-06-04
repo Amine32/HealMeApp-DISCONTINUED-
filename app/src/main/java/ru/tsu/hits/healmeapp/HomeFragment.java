e50876a87eec9c73101f23d7c483bcf6b31ab550
Amine32 <aminov.02@hotmail.com>
2022-06-04 23:20:52 +0700

    Created RequestMedicine Functionality  - Added possibility to request medicine  - Added possibility to search for medicine from database  - Added possibility to set quantity of medicine required  - Added possibility to add description  - Added possibility to add address  - Added RequestMedicineFragment to HomeFragment

--- app/src/main/java/ru/tsu/hits/healmeapp/HomeFragment.java
+++ app/src/main/java/ru/tsu/hits/healmeapp/HomeFragment.java
@@ -41,7 +41,7 @@ public class HomeFragment extends Fragment {
         btnRequestMedicine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(), RequestMedicine.class);
+                Intent intent = new Intent(getContext(), RequestMedicineMainFragment.class);
                 startActivity(intent);
             }
         });
